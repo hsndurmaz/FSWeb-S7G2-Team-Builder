3a07aa81cbbe5e09f7fb356aa7f5b6709fb88668
hsndurmaz <hsndurmaz3@gmail.com>
2023-08-08 23:57:39 +0300

    Continues

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [memberList, setMemberList] = useState([
+    { name: "Hasan", old: "25", major: "Civil Engineer" },
+    { name: "Gülşen", old: "24", major: "Maths" },
+    { name: "Emre", old: "25", major: "Computer Science" },
+    { name: "Selo", old: "25", major: "Agriculture Engineer" },
+  ]);
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>aaaa</p>
+        {memberList.map((member, i) => (
+          <p key={i}>
+            <strong> {member.name}</strong>-{member.major}-{member.old}
+          </p>
+        ))}
       </header>
     </div>
   );
